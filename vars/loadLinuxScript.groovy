1d5efbed4ac711ad9399d66cdf5151b364168ed1
umesh <umeshkothval@gmail.com>
2022-09-20 11:19:28 +0530

    corrected script path in loadLinuxScript.sh

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call (Map config = [:]) {
-def scriptcontent = libraryResource "$/scripts/linux/${config.name}"
+def scriptcontent = libraryResource "scripts/linux/${config.name}"
 writeFile file: "${config.name}" , text: scriptcontent
 sh "chmod a+x ./${config.name}"
 
